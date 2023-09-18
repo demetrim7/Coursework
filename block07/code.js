688f9d16833c094dbc09e7061614745cddcf9610
Demetri Martinez <demetrimartinez778@icloud.com>
2023-09-18 14:20:32 -0600

    push all work

---
+++ block07/code.js
@@ -0,0 +1,15 @@
+const string1 = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:" ;
+
+const x = 10
+const y = 40
+const z = 39
+
+console.log(x)
+// expected output 10
+
+console.log(y)
+// expected output 40
+
+console.log(z)
+// expected output 39
+console.log(x = y = z)
